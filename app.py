d4bffebe7931e7ee757f275d0f4a1236c6578c37
Lembley42 <paulverse@gmail.com>
2022-11-24 11:34:47 +0100

    test

--- app.py
+++ app.py
@@ -2,17 +2,41 @@ from flask import Flask, Blueprint, flash, g, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_minify import Minify, decorators as minify_decorators
-
+import redis, os
 
 app = Flask(__name__)
 CORS(app)
 
+redis_host = os.environ.get('REDIS_HOST', '')
+redis_port = os.environ.get('REDIS_PORT', '')
+redis_pw = os.environ.get('REDIS_PW', '')
+
+redis_db = redis.StrictRedis(host=redis_host, port=int(redis_host), password=redis_pw, ssl=True)
+redis_db.ping()
+
 bp = Blueprint('fullbanner', __name__, url_prefix='/cms/<id>')
+create = Blueprint('create', __name__, url_prefix='/create/<id>')
+
+
 app.register_blueprint(bp)
+app.register_blueprint(create)
+
 
 @app.route('/cms/<id>', methods=['GET'])
 def view(id):
-    if id == '1': 
-        return render_template('fullbanner_1.html')
-    elif id == '2':
-        return render_template('fullbanner_2.html')
+    return redis_db.get(id)
+
+
+@app.route('/create/<id>', methods=['GET'])
+def create(id):
+    headline = request.args.get('headline')
+
+    template = render_template('fullbanner_custom.html', headline=headline)
+    redis_db.set(id, template)
+    print(f'Created banner {id} with headline {headline}')
+    return f'Sucessfully created the template and stored it with Id {id}'
+
+
+if __name__ == '__main__':
+    app.run()
+
